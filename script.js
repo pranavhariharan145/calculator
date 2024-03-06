e3b3960fa36cda1ac77f3bd79686362bd6c5a435
Pranav <pranavhariharan145@gmail.com>
2024-03-06 11:05:14 +0530

    Done with basic HTML layout

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-function add(a,b)
-{
-    return a+b
+function add(a, b) {
+    return a + b
 }
 
-function subtract(a,b)
-{
-    return a-b
+function subtract(a, b) {
+    return a - b
 }
 
-function multiply(a,b)
-{
-    return a*b
+function multiply(a, b) {
+    return a * b
 }
 
-function divide(a,b)
-{
-    return a/b
+function divide(a, b) {
+
+    return a / b
+
 }
 
 
-function operate(num1, num2, operation)
-{
-    if (operation == "+")
-    {
-        return num1 + " + " + num2 + " = " + add(num1,num2)
+function operate(num1, num2, operation) {
+    if (operation == "+") {
+        return num1 + " + " + num2 + " = " + add(num1, num2)
     }
 
-    if (operation == "-")
-    {
-        return num1 + " - " + num2 + " = " + subtract(num1,num2)
+    if (operation == "-") {
+        return num1 + " - " + num2 + " = " + subtract(num1, num2)
     }
 
-    if (operation == "*")
-    {
-        return num1 + " * " + num2 + " = " + multiply(num1,num2)
+    if (operation == "*") {
+        return num1 + " * " + num2 + " = " + multiply(num1, num2)
     }
 
-    if (operation == "/")
-    {
-        return num1 + " / " + num2 + " = " + divide(num1,num2)
+    if (operation == "/") {
+        return num1 + " / " + num2 + " = " + divide(num1, num2)
     }
-   
+
 }
-let result = operate(100,2,"*")
+let result = operate(2, 0, "/")
 console.log(result)
