0fb8169d8e326654a5047b0de5f38fe163bad68a
Pranav <pranavhariharan145@gmail.com>
2024-03-06 09:08:10 +0530

    Done with a simple console application

---
+++ script.js
@@ -0,0 +1,46 @@
+function add(a,b)
+{
+    return a+b
+}
+
+function subtract(a,b)
+{
+    return a-b
+}
+
+function multiply(a,b)
+{
+    return a*b
+}
+
+function divide(a,b)
+{
+    return a/b
+}
+
+
+function operate(num1, num2, operation)
+{
+    if (operation == "+")
+    {
+        return num1 + " + " + num2 + " = " + add(num1,num2)
+    }
+
+    if (operation == "-")
+    {
+        return num1 + " - " + num2 + " = " + subtract(num1,num2)
+    }
+
+    if (operation == "*")
+    {
+        return num1 + " * " + num2 + " = " + multiply(num1,num2)
+    }
+
+    if (operation == "/")
+    {
+        return num1 + " / " + num2 + " = " + divide(num1,num2)
+    }
+   
+}
+let result = operate(100,2,"*")
+console.log(result)
